b5e450deff51d43dd26d5e9b156793a77bf08b45
Michael Walter <michael@MacBook-Pro.local>
2020-06-12 11:07:52 -0600

    Completed ring_buffer.py, all tests pass. Working on further optimizing names.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,44 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current_index = 0
+        self.buffer =[]
 
     def append(self, item):
-        pass
+        # if self.current_index > self.capacity:
+        #     if not self.buffer[self.current_index]:
+        #         self.buffer.append(item)
+        #         self.current_index += 1
+        #     else:
+        #         self.buffer[self.current_index] = item
+        # else:
+        #     self.current_index = 0
+        #-----------
+        # while len(self.buffer) < self.capacity:
+        #     self.buffer.append(None)
+        #
+        # if self.current_index < self.capacity:
+        #     if not self.buffer[self.current_index]:
+        #         self.buffer.append(item)
+        #     else:
+        #         self.buffer[self.current_index] = item
+        #         self.current_index += 1
+        # if self.current_index > (self.capacity - 1):
+        #     self.current_index = 0
+        #-----------
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+
+        self.buffer[self.current_index] = item
+
+        self.current_index += 1
+
+        if self.current_index > (self.capacity - 1):
+            self.current_index = 0
+
+        
+
+        
 
     def get(self):
-        pass
+        return self.buffer
